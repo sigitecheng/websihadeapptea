25dd9d0517c8bc774d82913bfc7a05c990b7c1cf
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-10-27 16:29:56 +0700

    df

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,68 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// ------------------------- FRONTEND HALAMAN UTAMA SIPJAKI BANDUNG BARAT --------------------------
+
+
+
+// Route::get('/portalberita', function () {
+//     // return view('welcome');
+//     return view('portalberita', [
+//         'title' => 'Portal Berita',
+//     ]);
+// });
+
+
+Route::get('/404', function () {
+    // return view('welcome');
+    return view('404', [
+        'title' => 'Under Constructions',
+    ]);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/bahan2', function () {
+    // return view('welcome');
+    return view('frontend.00_full.bahan2');
 });
+
+
+// Route::get('/$login', function () {
+//     // return view('welcome');
+//     return view('login.index',
+//         'title' => 'Halaman Login'
+//     ]);
+// });
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');  
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__.'/auth.php';
